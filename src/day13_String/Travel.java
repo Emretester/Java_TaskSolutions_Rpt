1c1cb737a075235d82eaf3f6514fdbdf60ccd8cc
Emretester <emreyeni2019@gmail.com>
2023-12-01 00:28:14 +0300

    day13 finished

--- src/day13_String/Travel.java
+++ src/day13_String/Travel.java
@@ -1,9 +1,66 @@
 package day13_String;
 
+import java.util.Scanner;
+
 public class Travel {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Do you have a valid passport (yes or no)?");
+        String yesOrNo = scan.next().toLowerCase();
+
+
+        if (yesOrNo.equals("yes")) { // if the user has passport
+            int cost = 1000;
+            scan.nextLine();
+            System.out.println("Which country are you traveling too?");
+            String country = scan.nextLine();
+            System.out.println("How many bags do you have?");
+            int numberOfBags = scan.nextInt();
+            cost += 50 * numberOfBags;
+            System.out.println("how many people do you travel with");
+            int numOfPeople = scan.nextInt();
+            if (numOfPeople < 3) {
+                cost -= 100 * numOfPeople;
+            } else {
+                cost -= 300;
+            }
+            scan.nextLine();
+            System.out.println("" +
+                    "");
+            String names = scan.nextLine();
+
+            System.out.println("Your ticket is booked to " + country +
+                    ". We have charged extra for the " + numberOfBags +
+                    " bags, but you are traveling with " + numOfPeople +
+                    " people so we are giving a discount. Your total cost is $" + cost);
+
+        } else {// if user does not have a valid passport
+            int cost = 200;
+            System.out.println("When did your passport expire?");
+            int expiredYear = scan.nextInt();
+            cost += 75 * (2021 - expiredYear);
+            scan.nextLine();
+            System.out.println("Which country are you traveling too?");
+            String country = scan.nextLine();
+            System.out.println("Will you be traveling in the next year (yes or no)?");
+            String answer = scan.next().toLowerCase();
+
+            if (answer.equals("yes")) {
+                cost += 100;
+            } else {
+                cost -= 50;
+            }
+
+            System.out.println("Looks like your password has been expired for " + (2021 - expiredYear)
+                    + ", but not to worry we will get it ready for you to travel to " + country
+                    + ". Your total cost has come out to $" + cost);
+
+        }
+
+        scan.close();
 
     }
 }
